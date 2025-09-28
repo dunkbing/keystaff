8985dbf9715244bf8790d7691b0f19bc0376ce73
binh.buidang <dangbinh4869@gmail.com>
2025-09-28 19:57:58 +0700

    init

---
+++ jisho/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  jisho
+//
+//  Created by Bùi Đặng Bình on 28/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ jisho/jishoApp.swift
@@ -0,0 +1,17 @@
+//
+//  jishoApp.swift
+//  jisho
+//
+//  Created by Bùi Đặng Bình on 28/9/25.
+//
+
+import SwiftUI
+
+@main
+struct jishoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
